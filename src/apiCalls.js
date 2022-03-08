93c711856e502dfd3eb8c38b33f5f7689bbe1a87
Kayla Durrett <kayla.durrett@gmail.com>
2022-03-08 09:35:14 -0700

    Add post request to add an order in apiCalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,13 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+export const postToOrders = (order) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    body: JSON.stringify({ name: order.name, ingredients: order.ingredients }),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  }).then(response => response.json())
+}
